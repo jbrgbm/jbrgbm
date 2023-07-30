45a3f98918d97dc2ae049b8ec84a0608506be8d4
jbrgbm <46921080+jbrgbm@users.noreply.github.com>
2023-07-30 23:32:53 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x172ccd798dc872d48c24421c4b0a8fcbab958eda";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
